5b7d89b4d7fe02eb6af2f059ca1370551d840dcb
Juan Orozco <ingjuanorozco@outlook.com>
2020-03-26 10:40:09 -0500

    generate Allure report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/createpost.feature",
+@CucumberOptions(features = "src/test/resources/features/",
                 plugin = { "pretty",
                         "html:target/site/cucumber-pretty",
                         "json:target/cucumber.json" },
